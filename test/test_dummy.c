5a49b03734915a633139d62157a8c09ec03b6d5f
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2023-08-07 17:17:16 +0200

    testeo de workflow

--- test/test_dummy.c
+++ test/test_dummy.c
@@ -16,8 +16,7 @@ static void test_fail(void **state) {
 
 int main(void) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_nothing),
-        //cmocka_unit_test(test_fail),
+        cmocka_unit_test(test_nothing)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
