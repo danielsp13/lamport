0da33534b92f1462c4a64d1a0648e60653e4d2b7
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2023-08-26 23:43:19 +0200

    :sparkles: tenemos compiladorgit add src/lmp_analysis.c src/lmp_io.c src/lmp.c ! #84

---
+++ include/lmp_analysis.h
@@ -0,0 +1,89 @@
+/**
+ * LAMPORT. Simulador de Sistemas Concurrentes y Distribuidos
+ * @file lmp_analysis.h
+ * @author Daniel Perez Ruiz
+ * @brief 
+ */
+
+#ifndef _LMP_COMPILER_ANALYSIS_DPR_
+#define _LMP_COMPILER_ANALYSIS_DPR_
+
+// ===============================================================
+
+// ----- DEFINICION DE MACROS PARA GESTION DE ANALISIS -----
+
+#define LMP_PARSING_SUCCESS 0       ///< Indica el exito en el parsing de un fichero de lamport
+#define LMP_PARSING_FAILURE -1      ///< Indica el fallo en el parsing de un fichero de lamport
+
+#define LMP_PARSING_ERROR_MSG_HEADER "--- [LMP: PARSING - ERROR]"
+#define LMP_PARSING_ERROR_MSG "Error en el procesamiento del fichero"
+
+// ===============================================================
+
+// ----- INCLUSION DE DEPENDENCIAS DE C -----
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ===============================================================
+
+// ----- INCLUSION DE MODULOS -----
+
+#include "AST/AST.h"            ///< Abstract Syntax Tree (AST)
+
+// ===============================================================
+
+// ----- VARIABLES GLOBALES DE GESTION (AST) -----
+
+/**
+ * @brief AST del programa lamport reconocido por el analizador sintactico
+ */
+extern struct program * program;
+
+// ===============================================================
+
+// ----- IMPORTACION DE FUNCIONES Y VARIABLES EXTERNAS (FLEX/BISON) -----
+
+/**
+ * @brief Funcion generada por Flex. Se encarga de realizar el analisis
+ * lexico, reconociendo tokens a partir del texto fuente y retornando el
+ * tipo de token reconocido.
+ * 
+ * Utilizada por yyparse()
+ */
+extern int yylex(void);
+
+/**
+ * @brief Funcion generada por Bison. Realiza el analisis sintactico del texto
+ * fuente, estructurando los tokens reconocidos en la fase anterior por yylex().
+ * @param program : Puntero a AST del programa
+ */
+extern int yyparse(void);
+
+// ===============================================================
+
+// ----- PROTOTIPO DE FUNCIONES DE GESTION (PARSING) -----
+
+/**
+ * @brief Realiza el analisis lexico y sintactico del fichero de lamport
+ * abierto por el compilador
+ * @return LMP_PARSING_SUCCESS si exito, LMP_PARSING_FAILURE en otro caso
+ */
+int lmp_parsing_file();
+
+// ===============================================================
+
+// ----- PROTOTIPO DE FUNCIONES DE GESTION (AST) -----
+
+/**
+ * @brief Imprime el AST generado por el analizador sintactico
+ */
+void lmp_print_AST();
+
+/**
+ * @brief Libera la memoria del AST generado por el analizador sintactico
+ */
+void lmp_free_AST();
+
+#endif //_LMP_COMPILER_ANALYSIS_DPR_
---
+++ src/lmp.c
@@ -0,0 +1,74 @@
+/**
+ * LAMPORT. Simulador de Sistemas Concurrentes y Distribuidos
+ * @file lmp.c
+ * @author Daniel Perez Ruiz
+ * @brief Programa principal del compilador de lamport
+ */
+
+// ===============================================================
+
+// ----- INCLUSION DE DEPENDENCIAS DE C -----
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ===============================================================
+
+// ----- INCLUSION DE MODULOS -----
+
+#include "AST/AST.h"    ///< Importacion del AST
+
+// ===============================================================
+
+// ----- INCLUSION DE COMPONENTES DE GESTION DE COMPILADOR -----
+
+#include "lmp_io.h"         ///< Gestion de entrada/salida de lmp
+#include "lmp_analysis.h"   ///< Parsing de fichero de lmp
+
+// ===============================================================
+
+// ----- PROGRAMA PRINCIPAL -----
+
+int main(int argc, char **argv){
+    // -- 0. Comprobar numero de argumentos
+    if(argc != N_ARGUMENTS_REQUIRED){
+        // -- Imprimir mensaje de uso
+        print_help(argv[0]);
+        // -- Salir con error
+        exit(EXIT_FAILURE);
+    }
+
+    // -- 1. Abrir fichero 
+    if(abrir_fichero(argv[1]) < LMP_IO_SUCCESS){
+        // -- Salir con error
+        exit(EXIT_FAILURE);
+    }
+
+    // ---------------------------------------------------------
+
+    // -- 2. Realizar parsing
+    if(lmp_parsing_file() < LMP_IO_SUCCESS){
+        // -- Imprimir mensaje de error
+        fprintf(stderr,"%s %s : %s\n", LMP_PARSING_ERROR_MSG_HEADER, LMP_PARSING_ERROR_MSG, file_parsed_name);
+    }
+    else{
+        fprintf(stdout,"ANALISIS DE %s completado con exito!!!\n", file_parsed_name);
+    }
+    
+
+    // -- . Cerrar fichero
+    if(cerrar_fichero() < LMP_IO_SUCCESS){
+        // -- Salir con error
+        exit(EXIT_FAILURE);
+    }
+
+    // -- . Imprimir AST
+    lmp_print_AST();
+
+    // -- . Liberar memoria utilizada para el AST
+    lmp_free_AST();
+
+    // -- Retorno
+    return EXIT_SUCCESS;
+}
