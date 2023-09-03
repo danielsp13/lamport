85c88a0d3f6b8b1af8fe67c9464460f13eef27d9
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2023-09-03 18:25:37 +0200

    :sparkles: simbolo (symbol), #89

---
+++ include/semantic/symbol.h
@@ -0,0 +1,115 @@
+/**
+ * LAMPORT. Simulador de Sistemas Concurrentes y Distribuidos
+ * @file symbol.h
+ * @author Daniel Perez Ruiz
+ * @brief Definicion de estructuras y funciones para creacion y mantenimiento
+ * de un simbolo
+ */
+
+#ifndef _LAMPORT_SEMANTIC_SYMBOL_DPR_
+#define _LAMPORT_SEMANTIC_SYMBOL_DPR_
+
+// ===============================================================
+
+// ----- INCLUSION DE DEPENDENCIAS DE NODO -----
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#include "AST/type.h"           ///< Tipos
+
+// ===============================================================
+
+// ----- DEFINICION DE TIPOS DE SIMBOLOS ----
+
+/**
+ * @brief Estructura que representa los tipos de simbolos del lenguaje lamport
+ */
+typedef enum{
+    SYMBOL_LOCAL,       ///< El simbolo es una variable local
+    SYMBOL_PARAM,       ///< El simbolo es el parametro de una funcion/procedimiento
+    SYMBOL_GLOBAL       ///< El simbolo es una variable global
+} symbol_t;
+
+// ===============================================================
+
+// ----- DEFINICION DE ESTRUCTURA SIMBOLO -----
+
+/**
+ * @brief Estructura que representa un simbolo de lamport
+ * 
+ * Esta estructura almacena informacion sobre un simbolo, que representa a la declaracion
+ * de una variable en el lenguaje lamport. Consta de un tipo, un puntero al tipo de dato
+ * del simbolo, el nombre del mismo y, en caso de que se trate de un simbolo de un
+ * parametro de una funcion/procedimiento, la posicion que este ocupa.
+ */
+struct symbol{
+    symbol_t kind;          ///< Tipo de simbolo
+    char * kind_str;        ///< Tipo de simbolo (en formato string)
+    struct type * type;     ///< Tipo de dato del simbolo
+    char * name;            ///< Nombre del simbolo
+    int which;              ///< Posicion en la lista de parametros (solo si kind = SYMBOL_PARAM)
+};
+
+// ===============================================================
+
+// ----- PROTOTIPO DE FUNCIONES DE CONSTRUCCION DE SIMBOLOS -----
+
+/**
+ * @brief Crea y reserva memoria para un simbolo
+ * @param kind : Tipo de simbolo
+ * @param type : Tipo de dato del simbolo
+ * @param name : Nombre del simbolo
+ * @param which : Posicion que ocupa en la lista de parametros (solo si kind = SYMBOL_PARAM)
+ * @return puntero a simbolo inicializado, NULL en otro caso
+ */
+struct symbol * create_symbol(symbol_t kind, struct type * type, char * name, int which);
+
+/**
+ * @brief Crea y reserva memoria para un simbolo local
+ * @param type : Tipo de dato del simbolo
+ * @param name : Nombre del simbolo
+ * @return puntero a simbolo inicializado, NULL en otro caso
+ */
+struct symbol * create_symbol_local(struct type * type, char * name);
+
+/**
+ * @brief Crea y reserva memoria para un simbolo global
+ * @param type : Tipo de de dato del simbolo
+ * @param name : Nombre del simbolo
+ * @return puntero a simbolo inicializado, NULL en otro caso
+ */
+struct symbol * create_symbol_global(struct type * type, char * name);
+
+/**
+ * @brief Crea y reserva memoria para un simbolo de parametro de funcion/procedimiento
+ * @param type : Tipo de dato del simbolo
+ * @param name : Nombre del simbolo
+ * @param which : Posicion que ocupa en la lista de parametros
+ * @return puntero a simbolo inicializado, NULL en otro caso
+ */
+struct symbol * create_symbol_param(struct type * type, char * name, int which);
+
+// ===============================================================
+
+// ----- PROTOTIPO DE FUNCIONES DE LIBERACION DE MEMORIA DE SIMBOLOS -----
+
+/**
+ * @brief Libera la memoria asignada para un simbolo
+ * @param symb : Simbolo a liberar
+ */
+void free_symbol(struct symbol * symb);
+
+// ===============================================================
+
+// ----- PROTOTIPO DE FUNCIONES DE IMPRESION DE SIMBOLOS -----
+
+/**
+ * @brief Imprime la infomacion de un simbolo
+ * @param symb : Simbolo a imprimir
+ */
+void print_symbol(struct symbol * symb);
+
+#endif //_LAMPORT_SEMANTIC_SYMBOL_DPR_
---
+++ src/semantic/symbol.c
@@ -0,0 +1,119 @@
+/**
+ * LAMPORT. Simulador de Sistemas Concurrentes y Distribuidos
+ * @file symbol.c
+ * @author Daniel Perez Ruiz
+ * @brief Implementacion de estructuras y funciones para creacion y mantenimiento
+ * de un simbolo
+ */
+
+#include "semantic/symbol.h"
+
+// ===============================================================
+
+// ----- IMPLEMENTACION DE FUNCIONES DE CONSTRUCCION DE SIMBOLOS -----
+
+struct symbol * create_symbol(symbol_t kind, struct type * type, char * name, int which){
+    struct symbol * symb = malloc(sizeof(*symb));
+
+    // -- Comprobar reserva de memoria exitosa
+    if(!symb)
+        return NULL;
+
+    // -- Asignar tipo de simbolo (symbol_t y str)
+    symb->kind = kind;
+    switch (symb->kind)
+    {
+    case SYMBOL_LOCAL:
+        symb->kind_str = strdup("SYMBOL_LOCAL");
+        break;
+    
+    case SYMBOL_GLOBAL:
+        symb->kind_str = strdup("SYMBOL_GLOBAL");
+        break;
+
+    case SYMBOL_PARAM:
+        symb->kind_str = strdup("SYMBOL_PARAM");
+        break;
+    }
+    // -- Comprobar asignacion de tipo de simbolo (str) exitosa
+    if(!symb->kind_str){
+        // -- Liberar memoria reservada para simbolo
+        free(symb);
+        return NULL;
+    }
+
+    // -- Asignar tipo de dato de simbolo
+    symb->type = type;
+
+    // -- Asignar nombre de variable de simbolo
+    symb->name = strdup(name);
+    // -- Comprobar asignacion de nombre exitosa
+    if(!symb->name){
+        // -- Liberar memoria reservada para simbolo
+        free(symb);
+        return NULL;
+    }
+
+    // -- Asignar posicion en la lista de parametros de simbolo
+    symb->which = which;
+
+    // -- Retornar simbolo creado e inicializado
+    return symb;
+}
+
+struct symbol * create_symbol_local(struct type * type, char * name){
+    return create_symbol(SYMBOL_LOCAL, type, name, -1);
+}
+
+struct symbol * create_symbol_global(struct type * type, char * name){
+    return create_symbol(SYMBOL_GLOBAL, type, name, -1);
+}
+
+struct symbol * create_symbol_param(struct type * type, char * name, int which){
+    return create_symbol(SYMBOL_PARAM, type, name, which);
+}
+
+// ===============================================================
+
+// ----- IMPLEMENTACION DE FUNCIONES DE LIBERACION DE MEMORIA DE SIMBOLOS -----
+
+void free_symbol(struct symbol * symb){
+    // -- Comprobar que symb apunta a algo
+    if(!symb)
+        return;
+
+    // -- Liberar memoria de tipo de simbolo (str)
+    if(symb->kind_str)
+        free(symb->kind_str);
+
+    // -- Liberar memoria de nombre de variable
+    if(symb->name)
+        free(symb->name);
+
+    // -- Liberar simbolo
+    free(symb);
+}
+
+// ===============================================================
+
+// ----- IMPLEMENTACION DE FUNCIONES DE IMPRESION DE SIMBOLOS -----
+
+void print_symbol(struct symbol * symb){
+    const char *IDENT_ARROW = "== ";
+
+    // -- Comprobar que symb apunta a algo
+    if(!symb){
+        printf(" %s <NONE>\n", IDENT_ARROW);
+        return;
+    }
+
+    // -- Imprimir tipo y nombre de simbolo
+    printf(" %s SIMBOLO DE TIPO: [%s] DE NOMBRE: [%s]\n", IDENT_ARROW, symb->kind_str, symb->name);
+    // -- Imprimir posicion de la lista de parametros (si kind es acorde)
+    if(symb->kind == SYMBOL_PARAM)
+        printf(" %s POSICION EN LA LISTA DE PARAMETROS DE PROCEDENCIA: [%d]\n", IDENT_ARROW, symb->which);
+
+    // -- Imprimir tipo de dato simbolo
+    printf(" %s INFORMACION DEL TIPO DE DATO DE SIMBOLO: [%s]\n", IDENT_ARROW, symb->name);
+    print_AST_type(symb->type);
+}
